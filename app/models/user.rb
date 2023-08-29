c53d162209c68cfdab5f8dd024286c755031a814
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-08-29 23:21:29 +0900

    [add]ゲストログイン機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   validates :name, presence: true
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   get '/users/profile', to: 'users#profile'
   patch '/users/profile', to: 'users#update_profile'
   resources :users, only: [:show]
