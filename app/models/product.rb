3a96726b675af566a7cfb161e1852d03bf3fb50f
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-09-05 04:22:54 +0900

    [add]製品画像の件数制限を追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,22 @@
 class Product < ApplicationRecord
-  has_many_attached :images
+  FILE_NUMBER_LIMIT = 3
   
   belongs_to :user
+  has_many_attached :images
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   validates :name, presence: true
   validates :description, presence: true
+  validate :validate_number_of_files
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
- end
+  end
+
+  private
+  def validate_number_of_files
+    return if images.length <= FILE_NUMBER_LIMIT
+    errors.add(:images, "に添付できる画像は#{FILE_NUMBER_LIMIT}枚までです。")
+  end
 end
