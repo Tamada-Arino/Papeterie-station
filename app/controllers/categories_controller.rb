5945b04e7cc285f235e8e2178757b4e7fe93627d
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-09-09 23:07:00 +0900

    [add]カテゴリーに属する製品を取得するコードの追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
+    all_category_ids = @category.self_and_descendants.pluck(:id)
+    @products = Product.where(category_id: all_category_ids)
   end
 end
