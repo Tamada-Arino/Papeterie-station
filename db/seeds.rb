487117fd48b4d7af2eeb6384338265267df4611f
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-09-09 23:12:14 +0900

    [add]db/seeds.rbにCategoryテーブルの初期データを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+root_category = Category.create(name: "筆記用具", parent_id: nil)
+Category.create(name: "万年筆", parent_id: root_category.id)
+Category.create(name: "ボールペン", parent_id: root_category.id)
+Category.create(name: "シャープペンシル", parent_id: root_category.id)
+Category.create(name: "えんぴつ", parent_id: root_category.id)
+Category.create(name: "マーカー・サインペン", parent_id: root_category.id)
+Category.create(name: "筆・筆ペン", parent_id: root_category.id)
+Category.create(name: "修正液・修正テープ", parent_id: root_category.id)
+Category.create(name: "消しゴム", parent_id: root_category.id)
+Category.create(name: "その他", parent_id: root_category.id)
+
+Category.create(name: "ノート・紙製品", parent_id: nil)
+Category.create(name: "ペンケース", parent_id: nil)
+
+root_category = Category.create(name: "手帳", parent_id: nil)
+Category.create(name: "一般", parent_id: root_category.id)
+Category.create(name: "システム手帳", parent_id: root_category.id)
+Category.create(name: "手帳カバー", parent_id: root_category.id)
+Category.create(name: "その他", parent_id: root_category.id)
+
+Category.create(name: "ファイリング用品", parent_id: nil)
+
+root_category = Category.create(name: "切断用品", parent_id: nil)
+Category.create(name: "はさみ", parent_id: root_category.id)
+Category.create(name: "カッターナイフ", parent_id: root_category.id)
+Category.create(name: "ペーパーナイフ", parent_id: root_category.id)
+Category.create(name: "その他", parent_id: root_category.id)
+
+Category.create(name: "その他", parent_id: nil)
