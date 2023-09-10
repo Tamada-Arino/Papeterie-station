5f573a378f2b550ca1d478dfa89c70d44b8393f9
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-09-11 03:11:35 +0900

    [add]カテゴリーでのソート機能を実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,20 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     all_category_ids = @category.self_and_descendants.pluck(:id)
-    @products = Product.where(category_id: all_category_ids)
+
+    order = params[:order]
+    case order
+    when "new"
+      @order_text = "新しい順"
+      @products = Product.where(category_id: all_category_ids).order(created_at: :desc)
+    when "old"
+      @order_text = "古い順"
+      @products = Product.where(category_id: all_category_ids).order(created_at: :asc)
+    when "favorites"
+      @order_text = "いいねの数順"
+      @products = Product.joins(:favorites).where(category_id: all_category_ids).group(:id).order('COUNT(favorites.id) DESC')
+    else
+      @products = Product.where(category_id: all_category_ids)
+    end
   end
 end
