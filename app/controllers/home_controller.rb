65f56f081008c3d4d39a246f4ac859efee336989
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-09-14 01:32:55 +0900

    [fit]検索フォームをトップページに追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @q = Product.ransack(params[:q])
     @new_products = Product.order(created_at: :desc).limit(4)
     @popular_products = Product.joins(:favorites)
                           .group('products.id')
