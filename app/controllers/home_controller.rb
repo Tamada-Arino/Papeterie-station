bf49d5c5ed0abb4361a38003353173548e789d81
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-09-13 23:19:45 +0900

    [add]トップページに人気投稿表示機能を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.order(created_at: :desc).limit(4)
+    @new_products = Product.order(created_at: :desc).limit(4)
+    @popular_products = Product.joins(:favorites)
+                          .group('products.id')
+                          .order('COUNT(favorites.id) DESC')
+                          .limit(4)
   end
 end
