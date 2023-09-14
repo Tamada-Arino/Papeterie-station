988ed87bc42283ca01bdbd0a2ee0aaee2b58a643
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-09-14 21:36:08 +0900

    [fit]カテゴリーコントローラーの記述を変更

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,22 @@
 class CategoriesController < ApplicationController
   def show
-    @q = Product.ransack(params[:q])
     @category = Category.find(params[:id])
     all_category_ids = @category.self_and_descendants.pluck(:id)
 
     order = params[:order]
-    case order
-    when "old"
-      @order_text = "古い順"
-      @products = Product.where(category_id: all_category_ids).order(created_at: :asc)
-    when "favorites"
-      @order_text = "いいねが多い順"
-      @products = Product.joins(:favorites).where(category_id: all_category_ids).group(:id).order('COUNT(favorites.id) DESC')
-    else
-      @order_text = "新しい順"
-      @products = Product.where(category_id: all_category_ids).order(created_at: :desc)
+    @q = Product.ransack(params[:q])
+
+    @q.sorts = 'created_at desc' if params[:order] == 'new'
+    @q.sorts = 'created_at asc' if params[:order] == 'old'
+    @q.sorts = 'favorites_count desc' if params[:order] == 'favorites'
+
+    @order_text =
+      case params[:order]
+      when 'old' then '古い順'
+      when 'favorites' then 'いいねが多い順'
+      else '新しい順'
     end
+
+    @products = @q.result(distinct: true).where(category_id: all_category_ids)
   end
 end
