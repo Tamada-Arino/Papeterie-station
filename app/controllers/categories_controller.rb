2a6b0bbf90554e032606eb32364694c6a4bf2cc2
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-09-15 14:01:54 +0900

    [fit]カテゴリーコントローラーの記述を変更

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,20 +3,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     all_category_ids = @category.self_and_descendants.pluck(:id)
 
-    order = params[:order]
     @q = Product.ransack(params[:q])
-
-    @q.sorts = 'created_at desc' if params[:order] == 'new'
-    @q.sorts = 'created_at asc' if params[:order] == 'old'
-    @q.sorts = 'favorites_count desc' if params[:order] == 'favorites'
-
-    @order_text =
-      case params[:order]
-      when 'old' then '古い順'
-      when 'favorites' then 'いいねが多い順'
-      else '新しい順'
-    end
-
     @products = @q.result(distinct: true).where(category_id: all_category_ids)
   end
 end
