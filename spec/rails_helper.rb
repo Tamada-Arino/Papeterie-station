a15af13e3978d9f6187d3e981c9852c523accb40
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-04 05:32:05 +0900

    [add]rails_helperにfactory_botの設定追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,6 +61,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.includeFactoryBot::Syntax::Methods
+  
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
 end
