72ebfc08604ccdecdadfdfc7682514e1caddc231
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-06 19:49:33 +0900

    [fix]production.rbの記述を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,13 +119,16 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.raise_delivery_errors = true
-  ActionMailer::Base.delivery_method = :smtp
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://papeterie-station-7e4731a83553.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'papeterie-station-7e4731a83553.herokuapp.com',
+    :domain         => host,
     :authentication => :plain,
   }
 end
