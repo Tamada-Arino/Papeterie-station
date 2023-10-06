baeffc4d76b9f8ad54ec51df005d7822010152b1
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 03:20:11 +0900

    [add]userのシステムスペックを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
+  let(:user) { create(:user) }
+
   before do
-    driven_by(:rack_test)
+    sign_in user
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'メールアドレスを変更できること' do
+    visit edit_user_registration_path
+
+    fill_in 'Eメール', with: 'new_email@example.com'
+    fill_in '現在のパスワード', with: user.password
+
+    click_button '更新'
+
+    expect(page).to have_content('アカウント情報を変更しました。')
+  end
+
+  it '名前とプロフィールを変更できること' do
+    visit users_profile_path
+
+    fill_in '名前', with: 'new_name'
+    fill_in 'プロフィール', with: 'new_profile'
+
+    click_button '更新'
+
+    expect(page).to have_content('プロフィールが更新されました。')
+  end
 end
