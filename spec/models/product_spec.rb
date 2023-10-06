e958bcfab920e004b283e438e11beb5404c88bbf
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 01:12:46 +0900

    [add]productのmodelスペックを作成

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '製品情報の登録' do
+    it "name、description、price、user_idとcategory_idが存在すれば登録できること" do
+      product = build(:product)
+      expect(product).to be_valid
+    end
+  end
 end
