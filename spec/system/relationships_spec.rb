cb36da6c76b3b64e92e80c8fa58c4cdd83b391f6
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 05:17:53 +0900

    [add]フォロー機能のシステムスペック追加

--- spec/system/relationships_spec.rb
+++ spec/system/relationships_spec.rb
@@ -1,9 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "Relationships", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  describe "ユーザー詳細ページ" do
+    let(:user) { create(:user) }
+    let(:another_user) { create(:user, email: "another@email.com") }
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    context "未ログイン時" do
+      before do
+        visit user_path(user.id)
+      end
+
+      it "フォローボタンがあること" do
+        expect(page).to have_button('フォローする')
+      end
+
+      it "フォローボタンを押すとログインページに遷移すること" do
+        click_button 'フォローする'
+        expect(current_path).to eq new_user_session_path
+      end  
+    end
+
+    context "ログイン時" do
+      before do
+        sign_in user
+      end
+
+      it "自分のユーザーページにフォローボタンがないこと" do
+        visit user_path(user.id)
+        expect(page).to have_no_button('フォローする')
+      end
+
+      it "他の人のページにフォローボタンがあること" do
+        visit user_path(another_user.id)
+        expect(page).to have_button('フォローする')
+      end
+
+      it "他の人のページのフォローボタンを押すとフォロー解除ボタンが出ること" do
+        visit user_path(another_user.id)
+        click_button 'フォローする'
+        expect(page).to have_button('フォローを解除')
+      end
+    end
+  end
 end
