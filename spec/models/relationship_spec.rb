0bea8ea15213fcd4c73bdd3292a68849da0d121c
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 02:12:33 +0900

    [add]いいね機能のモデルスペック作成

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'フォロー登録' do
+    it 'follower_idとfollowed_idがあれば登録できること' do
+      relationship = build(:relationship)
+      expect(relationship).to be_valid
+    end
+  end
 end
