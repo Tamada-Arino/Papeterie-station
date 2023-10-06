f2c5cf015c2e2585ada0d6ae65b909076a5b29ca
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 01:11:49 +0900

    [fix]factory_botのproduct初期データを編集

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
   factory :product do
+    user
+    category
+
     name { "test" }
     description { "test" }
     price { 1 }
-    user_id { 1 }
-    category_id { 1 }
+    user_id { user.id }
+    category_id { category.id }
   end
 end
