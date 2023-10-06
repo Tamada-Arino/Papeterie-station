020618621b0021dd69544d644af681fd2c7bdf51
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 00:29:11 +0900

    [add]ユーザー登録が失敗するパターンのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,37 @@ RSpec.describe User, type: :model do
   describe 'ユーザー登録' do
     it "name、email、passwordとpassword_confirmationが存在すれば登録できること" do
       user = build(:user)
-      expect(user).to be_valid  # user.valid? が true になればパスする
+      expect(user).to be_valid
+    end
+
+    it "nameがなければ登録できないこと" do
+      user = build(:user, name: "")
+      expect(user).not_to be_valid
+    end
+
+    it "emailがなければ登録できないこと" do
+      user = build(:user, email: "")
+      expect(user).not_to be_valid
+    end
+
+    it "passwordがなければ登録できないこと" do
+      user = build(:user, password: "")
+      expect(user).not_to be_valid
+    end
+
+    it "password_confirmationがなければ登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      expect(user).not_to be_valid
+    end
+
+    it "passwordとpassword_confirmationが不一致だと登録できないこと" do
+      user = build(:user, password_confirmation: "222222")
+      expect(user).not_to be_valid
+    end
+
+    it "passwordとpassword_confirmationが6文字未満だと登録できないこと" do
+      user = build(:user, password: "11111", password_confirmation: "11111")
+      expect(user).not_to be_valid
     end
   end
 end
