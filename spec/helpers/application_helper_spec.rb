e8d6f85afd746214df442d0476247a265ad38423
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-06 21:37:09 +0900

    [add]application_helper.rbのテストコードを作成

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ApplicationHelper. For example:
-#
-# describe ApplicationHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ApplicationHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ページタイトル" do
+    it "渡した文字列を含んだページタイトルになっていること" do
+      expect(full_title("test")).to eq("test - Papeterie Station")
+    end
+
+    it "引数がnilのときにPapeterie Stationになること" do
+      expect(full_title(nil)).to eq("Papeterie Station")
+    end
+
+    it "引数に何も設定されてないときPapeterie Stationになること" do
+      expect(full_title("")).to eq("Papeterie Station")
+    end
+  end
 end
