a5acbe7fa1443693d7f567ac0a4899621c9120b5
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 02:21:54 +0900

    [add]ユーザーが自分自身をフォローできないようにRelationshipモデルにバリデーションを追加

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,14 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+  validate :cannot_follow_yourself
+
+  private
+
+  def cannot_follow_yourself
+    if follower == followed
+      errors.add(:base, "You cannot follow yourself")
+    end
+  end
 end
