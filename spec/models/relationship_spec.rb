6757e607cc3c6c161ea534e89dd6d9973dd34d12
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 02:43:09 +0900

    [add]relationshipのモデルスペックのテストケース追加

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -6,5 +6,36 @@ RSpec.describe Relationship, type: :model do
       relationship = build(:relationship)
       expect(relationship).to be_valid
     end
+
+    it 'follower_idがないとき登録できないこと' do
+      relationship = build(:relationship, follower_id: "")
+      expect(relationship).not_to be_valid
+    end
+
+    it 'followed_idがないとき登録できないこと' do
+      relationship = build(:relationship, followed_id: "")
+      expect(relationship).not_to be_valid
+    end
+
+    it 'ユーザーが自分自身をフォローできないこと' do
+      user = create(:user)
+      relationship = build(:relationship, follower: user, followed: user)
+      expect(relationship).not_to be_valid
+    end
+    
+    context '一意性の確認' do
+      before do
+        @follower = create(:user)
+        @followed = create(:user, email: "test2@gmail.com")
+        @relationship = build(:relationship, follower: @follower, followed: @followed)
+      end
+    
+      it 'フォローする人とフォローされる人が同じレコードが登録されないこと' do
+        @relationship.save
+    
+        another_relationship = build(:relationship, follower: @follower, followed: @followed)
+        expect(another_relationship).not_to be_valid
+      end
+    end
   end
 end
