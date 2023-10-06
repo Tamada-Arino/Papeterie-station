2f56afc59c6c49090bbe415710f15dc284546946
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 01:18:44 +0900

    [add]productモデルのテストケースを追加

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,5 +6,30 @@ RSpec.describe Product, type: :model do
       product = build(:product)
       expect(product).to be_valid
     end
+
+    it "nameがなければ登録できないこと" do
+      product = build(:product, name: "")
+      expect(product).not_to be_valid
+    end
+
+    it "priceがなければ登録できないこと" do
+      product = build(:product, price: "")
+      expect(product).not_to be_valid
+    end
+
+    it "descriptionがなくても登録できること" do
+      product = build(:product, description: "")
+      expect(product).to be_valid
+    end
+
+    it "user_idがなければ登録できないこと" do
+      product = build(:product, user_id: "")
+      expect(product).not_to be_valid
+    end
+
+    it "category_idがなくても登録できること" do
+      product = build(:product, category_id: "")
+      expect(product).to be_valid
+    end
   end
 end
