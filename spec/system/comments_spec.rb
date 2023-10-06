2f7f0df5c3b6f6fd86c2373aee4ade3279876e7d
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 06:13:22 +0900

    [add]commentのシステムスペックを作成

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -1,9 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  describe "製品詳細ページ" do
+    let(:user) { create(:user) }
+    let(:product) { create(:product, user_id: user.id) }
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    before do
+      visit product_path(product.id)
+    end
+
+    context "未ログイン時" do
+      it "コメント投稿ボタンを押すとログインページへ遷移すること" do
+        click_button 'コメントを投稿'
+        expect(current_path).to eq new_user_session_path
+      end
+    end
+    
+    context "ログイン時" do
+      before do
+        sign_in user
+        fill_in 'コメント', with: 'comment'
+        click_button 'コメントを投稿'
+      end
+    
+      it "コメントが投稿できること" do
+        expect(page).to have_content('comment')
+      end
+    
+      it "コメント削除ボタンがあること" do
+        expect(page).to have_button('削除')
+      end
+    
+      it "コメント削除ボタンを押すとコメントが削除できること" do
+        click_button '削除'
+        expect(page).to have_content('コメントを削除しました')
+      end
+    end
+  end
 end
