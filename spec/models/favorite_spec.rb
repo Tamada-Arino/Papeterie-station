6c85bb81da59f5df0c0ebf89ae3db61489f2eb61
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 01:47:00 +0900

    [add]favoriteのモデルスペックを追加

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'いいねの登録' do
+    it 'user_idとproduct_idが存在するときいいねの登録ができること' do
+      favorite = build(:favorite)
+      expect(favorite).to be_valid
+    end
+
+    it 'user_idがないときいいねの登録ができないこと' do
+      favorite = build(:favorite, user_id: "")
+      expect(favorite).not_to be_valid
+    end
+
+    it 'product_idが存在しないときいいねの登録ができないこと' do
+      favorite = build(:favorite, product_id: "")
+      expect(favorite).not_to be_valid
+    end
+
+    context '一意性の確認' do
+      before do
+        @favorite = build(:favorite)
+      end
+
+      it 'ユーザーが同じ製品にいいねできないこと' do
+        @favorite.save
+
+        another_favorite = build(:favorite, user: @favorite.user, product: @favorite.product)
+        expect(another_favorite).not_to be_valid
+      end
+    end
+  end
 end
