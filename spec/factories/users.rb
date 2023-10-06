1b509f4e010c61744153b911fc536ed2cd0322eb
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-07 05:19:48 +0900

    [add]userのテストデータにプロフィール文を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     name                 {"test"}
+    introduction         {"test"}
     email                 {"test@gmail.com"}
     password              {"111111"}
     password_confirmation {"111111"}
