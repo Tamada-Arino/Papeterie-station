1af5279e938aebdf97eafb78ad7c224309d88607
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-10-14 04:43:32 +0900

    [add]トップページにフォローしたユーザーの新着投稿を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,10 @@ class HomeController < ApplicationController
                           .order('COUNT(favorites.id) DESC')
                           .limit(4)
     @new_users = User.order(created_at: :desc).limit(5)
+    if user_signed_in?                      
+      @follower_products = Product.where(user: current_user
+                            .following_users).order(created_at: :desc)
+                            .limit(4)
+    end
   end
 end
