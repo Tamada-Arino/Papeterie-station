c370657e3d396242fe0cecdb6cbc6f1fefbc26cb
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-12-21 01:34:30 +0900

    [add]inquiryのモデルスペック追加

--- spec/factories/inquiries.rb
+++ spec/factories/inquiries.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :inquiry do
-    name { "MyString" }
-    address { "MyString" }
-    text { "MyText" }
+    name { "test" }
+    address { "test@gmail.com" }
+    text { "test" }
   end
 end
