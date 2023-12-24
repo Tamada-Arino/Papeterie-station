41bc4a86b65cefecb7fe75575e54e4980f5ccf98
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-12-24 11:43:05 +0900

    [fix]inquiryのバリデーションのメッセージ変更

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,5 +1,5 @@
 class Inquiry < ApplicationRecord
   validates :name, presence: true
   validates :text, presence: true
-  validates :address, format: { with: URI::MailTo::EMAIL_REGEXP, message: "有効なメールアドレスを入力してください" }, allow_blank: true
+  validates :address, format: { with: URI::MailTo::EMAIL_REGEXP, message: "は正しく入力してください" }, allow_blank: true
 end
