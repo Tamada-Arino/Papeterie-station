915fbfa9597b35bb7805a0fc63f76556ffb9d577
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2023-12-25 22:55:17 +0900

    [fix]テスト環境の設定を変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,10 +73,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  host = 'https://papeterie-station-7e4731a83553.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
