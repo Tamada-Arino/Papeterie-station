4175c9e718f378d9222243ae0eb4a110553dfca7
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2024-01-10 17:19:55 +0900

    [fix]トップページのテストを修正

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -9,43 +9,13 @@ RSpec.describe "Homes", type: :system do
         visit root_path
       end
 
-      it "ログインへのリンクがあること" do
-        within(".btn-contain") do
-          expect(page).to have_content("ログイン")
-        end
-      end
-
-      it "ログインへのリンクをクリックするとログインページへ遷移できること" do
-        within(".btn-contain") do
-          click_link "ログイン"
-        end
-        expect(current_path).to eq new_user_session_path
-      end
-
       it "アカウント新規登録へのリンクがあること" do
-        within(".btn-contain") do
-          expect(page).to have_content("新規登録")
-        end
+        expect(page).to have_content("ユーザー登録して投稿する")
       end
 
       it "アカウント新規登録へのリンクをクリックすると新規登録ページへ遷移できること" do
-        within(".btn-contain") do
-          click_link "新規登録"
-        end
+        click_link "ユーザー登録して投稿する"
         expect(current_path).to eq new_user_registration_path
-      end
-
-      it "ゲストログインへのリンクがあること" do
-        within(".btn-contain") do
-          expect(page).to have_content("ゲストログイン")
-        end
-      end
-
-      it "ゲストログインへのリンクを押すとゲストログインできること" do
-        within(".btn-contain") do
-          click_link "ゲストログイン"
-        end
-        expect(page).to have_content("ゲストユーザーとしてログインしました。")
       end
     end
 
@@ -55,10 +25,8 @@ RSpec.describe "Homes", type: :system do
         visit root_path
       end
 
-      it "ログイン、新規登録、ゲストログインのリンクがないこと" do
-        expect(page).to have_no_content("ログイン")
-        expect(page).to have_no_content("新規登録")
-        expect(page).to have_no_content("ゲストログイン")
+      it "新規登録ページへのリンクがないこと" do
+        expect(page).to have_no_content("ユーザー登録して投稿する")
       end
     end
   end
