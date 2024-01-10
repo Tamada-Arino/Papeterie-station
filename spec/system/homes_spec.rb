f699bbcb0021c04d4703a8c0bc773ea504874485
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2024-01-10 18:37:36 +0900

    [add]トップページのテストケースを追加

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -28,6 +28,29 @@ RSpec.describe "Homes", type: :system do
       it "新規登録ページへのリンクがないこと" do
         expect(page).to have_no_content("ユーザー登録して投稿する")
       end
+
+      it "投稿作成ページへのリンクがあること" do
+        expect(page).to have_content("製品を投稿")
+      end
+
+      it "投稿作成ページのリンクをクリックすると該当ページへ遷移できること" do
+        click_link "製品を投稿"
+        expect(current_path).to eq new_product_path
+      end
+
+      it "お気に入り一覧へのリンクがあること" do
+        expect(page).to have_content("お気に入り一覧")
+      end
+
+      it "お気に入り一覧へのリンクをクリックするとユーザーページへ遷移できること" do
+        click_link "お気に入り一覧"
+        expect(current_path).to eq user_path(user.id)
+      end
+
+      it "お気に入り一覧へのリンクをクリックすると遷移先でお気に入りタブがアクティブであること" do
+        click_link "お気に入り一覧"
+        expect(page).to have_css("a.nav-link.active", text: "お気に入り一覧")
+      end
     end
   end
 end
