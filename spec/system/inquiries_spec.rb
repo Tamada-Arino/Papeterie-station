33ff7cd592f9b11235620b63775b6ebe77b607ca
Tamada-Arino <129414368+Tamada-Arino@users.noreply.github.com>
2024-01-12 11:10:27 +0900

    [fix]システムスペックの記述を最適化

--- spec/system/inquiries_spec.rb
+++ spec/system/inquiries_spec.rb
@@ -6,20 +6,17 @@ RSpec.describe "Inquiries", type: :system do
 
     before do
       visit new_inquiry_path
-    end
-
-    it "問い合わせ内容を入力し確認画面へボタンを押すと確認画面へ移れること" do
       fill_in "お名前", with: inquiry.name
       fill_in "メールアドレス", with: inquiry.address
       fill_in "お問い合わせ内容", with: inquiry.text
+    end
+
+    it "問い合わせ内容を入力し確認画面へボタンを押すと確認画面へ移れること" do
       click_button "確認画面へ"
       expect(current_path).to eq confirm_inquiries_path
     end
 
     it "問い合わせ内容が送信できること" do
-      fill_in "お名前", with: inquiry.name
-      fill_in "メールアドレス", with: inquiry.address
-      fill_in "お問い合わせ内容", with: inquiry.text
       click_button "確認画面へ"
       click_button "問い合わせ内容を送信する"
       expect(page).to have_content("問い合わせ内容を送信しました。")
